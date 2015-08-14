c420c3e505d937d836e1a69c6d2dac485c18287f
Nexen23 <tsalexn@mail.ru>
2015-08-14 22:44:22 +0700

    Fixed bugs with Queries in server side code

--- app/I_am_here/cloud/main.js
+++ app/I_am_here/cloud/main.js
@@ -5,8 +5,8 @@ var _ = require('underscore'); // jshint ignore:line
 var moment = require('moment'); // jshint ignore:line
 
 // Constants
-var sessionObjName = "Session";
-var sessionLifetimeSec = 30;
+var sessionObjName = "IMH_Session";
+var sessionLifetimeSec = 61;
 
 var channelName = "events";
 var publishKey = "pub-c-6271f363-519a-432d-9059-e65a7203ce0e",
@@ -22,7 +22,7 @@ function Log(obj, tag) {
     if (tag != null) { // jshint ignore:line
         loggingString += "[" + tag + "] ";
     }
-    loggingString += obj + "\n";
+    loggingString += JSON.stringify(obj) + "\n";
 
     console.log(loggingString); // jshint ignore:line
 }
@@ -69,20 +69,26 @@ function NewSession(udid) {
     return session;
 }
 
+function GetSessionQuery()
+{
+    "use strict";
+    var objConstructor = Parse.Object.extend(sessionObjName); // jshint ignore:line
+    return new Parse.Query(objConstructor);
+}
+
 function NewParseObject(session) {
     "use strict";
 
-    var obj = Parse.Object.extend(sessionObjName); // jshint ignore:line
-    Log(obj);
+    var objConstructor = Parse.Object.extend(sessionObjName); // jshint ignore:line
+    var obj = new objConstructor();
+
     obj.set({
         udid: session.udid,
-        loginedAt: session.loginedAt,
-        aliveTo: session.aliveTo
-        },
-        {
-            error: errorHandler
+        loginedAt: session.loginedAt.toDate(),
+        aliveTo: session.aliveTo.toDate()
         }
     );
+    Log(obj, "func- NewParseObject");
 
     return obj;
 }
@@ -94,7 +100,7 @@ function SendEvent(udid) {
         url: httpRequestUrl + JSON.stringify({udid: udid}),
 
         success: function(httpResponse) {
-            Log(httpResponse.text);
+            //Log(httpResponse.text);
         },
 
         error: function(httpResponse) {
@@ -114,7 +120,7 @@ var API_GetOnlineUsers = function(request, response) {
     "use strict";
 
     DeleteDeadSessions();
-    var query = new Parse.Query(sessionObjName); // jshint ignore:line
+    var query = GetSessionQuery();
 
     response.success( query.toJSON() );
 };
@@ -122,8 +128,8 @@ var API_GetOnlineUsers = function(request, response) {
 var API_IsUserOnline = function(request, response) {
     "use strict";
 
-    var userUdid = request.object.data.udid;
-    var query = new Parse.Query(sessionObjName) // jshint ignore:line
+    var userUdid = request.params.udid;
+    var query = GetSessionQuery()
         .equalTo("udid", userUdid)
         .lessThanOrEqualTo("aliveTo", GetNow());
 
@@ -144,8 +150,8 @@ var API_IsUserOnline = function(request, response) {
 var API_GetUserSession = function(request, response) {
     "use strict";
 
-    var userUdid = request.object.get("data").udid;
-    var query = new Parse.Query(sessionObjName); // jshint ignore:line
+    var userUdid = request.params.udid;
+    var query = GetSessionQuery();
     var promise = query.first("udid", userUdid);
     promise.then(function(result) {
         response.success(result);
@@ -157,51 +163,79 @@ var API_GetUserSession = function(request, response) {
 var API_Login = function(request, response) {
     "use strict";
 
-    var userUdid = request.params.data.udid;
+    var userUdid = request.params.udid;
     var session = NewSession(userUdid);
     var parseObject = NewParseObject(session);
 
-    parseObject.save().then(
-        function(success){},
-        function(error) {
-
-        }
-    );
-    Log(parseObject, "API_Login - parseObject");
+    parseObject.save(null, {
+        success: function(obj){Log(obj); response.success( JSON.stringify(parseObject) );},
+        error: function(error) {errorHandler(error); response.error();}
+    });
 
     // TODO: make UDID unique (+ can't login if already loginned)
-
-    response.success( JSON.stringify(parseObject) );
+    // TODO: not correct. Need another method. 3 types of sessions exist: NULL | DEAD | ALIVE
 };
 
 var API_Logout = function(request, response) {
     "use strict";
 
-    var userUdid = request.object.get("udid");
-    var query = new Parse.Query(sessionObjName) // jshint ignore:line
-        .equalTo("udid", userUdid);
+    var userUdid = request.params.udid;
+
+    var objConstructor = Parse.Object.extend(sessionObjName); // jshint ignore:line
+    var query = new Parse.Query(objConstructor);
+
+    query.equalTo("udid", userUdid);
+    query.find({
+        success: function(results) {
+            Log("Successfully retrieved " + results.length + " scores.");
+            // Do something with the returned Parse.Object values
+            for (var i = 0; i < results.length; i++) {
+                var object = results[i];
+                Log(object.id + ' - ' + object.get('playerName'));
+            }
+        },
+        error: function(error) {
+            Log("Error: " + error.code + " " + error.message);
+        }
+    });
+
+    /*query.each( function(obj) {
+        Log(obj, "Destroy2");
+        obj.destroy();
+    });*/
+
+    /*var query = GetSessionQuery();
+       // .equalTo("udid", userUdid);
+
 
     query.each( function(obj) {
-       obj.destroy();
+        Log(obj, "Destroy1");
+        obj.destroy();
     });
+
+    var userOnline = true;
+    query.count( {
+        success: function(number) {
+            Log(number, "Number of users");
+            if (number === 0) {
+                userOnline = false;
+            }
+        },
+        error: errorHandler
+    });
+    Log(userOnline, "UserOnline");
+
+
+    query.each( function(obj) {
+        Log(obj, "Destroy2");
+       obj.destroy();
+    });*/
 
     response.success();
 };
 
 
 // Bindings
-Parse.Cloud.beforeSave(sessionObjName, function(request, response) { // jshint ignore:line
-    "use strict";
-
-
-    // TODO: not correct. Need another method. 3 types of sessions exist: NULL | DEAD | ALIVE
-    var userUdid = request.object.get("udid");
-    Parse.Cloud.run("IsUserOnline", {data:{udid:userUdid}}, { // jshint ignore:line
-        success: response.success(),
-        error: response.error()
-    });
-});
-
 Parse.Cloud.afterSave(sessionObjName, function(request) { // jshint ignore:line
     "use strict";
 

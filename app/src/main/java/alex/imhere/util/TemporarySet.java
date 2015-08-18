5c48750917e369a6de45ecb7098a4319e89c4d67
Nexen23 <tsalexn@mail.ru>
2015-08-18 20:51:58 +0700

    implement Observable

--- app/src/main/java/alex/imhere/util/TemporarySet.java
+++ app/src/main/java/alex/imhere/util/TemporarySet.java
@@ -5,14 +5,19 @@ import org.joda.time.DateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Observable;
 import java.util.SortedSet;
 import java.util.Timer;
+import java.util.TimerTask;
 
-public class TemporarySet<T extends Object> {
+public class TemporarySet<T> extends Observable {
 	// TODO: 18.08.2015 make sure it's thread-safe implementation
 	protected SortedSet<TemporaryElement<T>> sortedElementsSet;
-	protected ArrayList<T> list;
-	protected Timer timer = new Timer();
+	protected List<T> list = new ArrayList<>();
+
+	protected final Timer timer = new Timer();
+	protected TimerTask timerTask = null;
+	protected TemporaryElement<T> nextElementToDie = null;
 
 	public boolean add(T object, DateTime deathTime) {
 		TemporaryElement<T> element = new TemporaryElement<>(object, deathTime);
@@ -28,25 +33,68 @@ public class TemporarySet<T extends Object> {
 		return Collections.unmodifiableList(list);
 	}
 
-	private synchronized boolean _add(TemporaryElement<T> element) {
+	private synchronized boolean _add(TemporaryElement<T> isertingElement) {
 		// TODO: 18.08.2015 implement unique add to List
-		boolean wasAdded = sortedElementsSet.add(element);
+		boolean wasAdded = sortedElementsSet.add(isertingElement);
 
 		if (wasAdded) {
-			list.add(0, element.object);
+			list.add(0, isertingElement.object);
+
+			if (nextElementToDie != null &&
+					nextElementToDie.deathTime.isAfter(isertingElement.deathTime)) {
+				cancelNextDeath();
+			}
+
+			if (nextElementToDie == null) {
+				openNextDeath();
+			}
+
+			notifyObservers();
 		}
 
 		return wasAdded;
 	}
 
-	private synchronized boolean _remove(TemporaryElement<T> element) {
+	private synchronized boolean _remove(TemporaryElement<T> deletingElement) {
 		// TODO: 18.08.2015 implement unique add to List
-		boolean wasRemoved = sortedElementsSet.remove(element);
+		boolean wasRemoved = sortedElementsSet.remove(deletingElement);
 
 		if (wasRemoved) {
-			list.remove(element.object);
+			list.remove(deletingElement.object);
+
+			if (nextElementToDie.equals(deletingElement)) {
+				cancelNextDeath();
+
+				if (sortedElementsSet.size() != 0) {
+					_add( sortedElementsSet.first() );
+				}
+
+			}
+
+			notifyObservers();
 		}
 
 		return wasRemoved;
+	}
+
+	private synchronized void openNextDeath() {
+		nextElementToDie = sortedElementsSet.first();
+		timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				killNextElement();
+			}
+		};
+	}
+
+	private synchronized void cancelNextDeath() {
+		timerTask.cancel();
+		timer.purge();
+		nextElementToDie = null;
+		timerTask = null;
+	}
+
+	private synchronized void killNextElement() {
+		_remove(nextElementToDie);
 	}
 }

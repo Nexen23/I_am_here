5cb3a99eefacaeb0092a9975d485ab548f526120
Nexen23 <tsalexn@mail.ru>
2015-08-25 00:18:14 +0700

    made UserView as root Layout for users

--- app/src/main/java/alex/imhere/fragment/view/UserLayout.java
+++ app/src/main/java/alex/imhere/fragment/view/UserLayout.java
@@ -12,14 +12,14 @@ import android.graphics.RectF;
 import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.View;
+import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 
 import alex.imhere.R;
 import alex.imhere.util.ArrayUtils;
 
-public class UserView extends View {
+public class UserLayout extends FrameLayout {
 	static public final int LIFETIME_DEAFULT = 2300;
 	static public final int[] COLORS_DEFAULT = {Color.WHITE, Color.BLACK};
 
@@ -34,20 +34,20 @@ public class UserView extends View {
 	private long accumulatorMs = 0;
 	private float gradientOffset = 0f;
 
-	public UserView(Context context) {
+	public UserLayout(Context context) {
 		super(context);
 		onInitialize();
 		// TODO: 24.08.2015 pass statesColors as args for View in XML
 	}
 
-	public UserView(Context context, AttributeSet attrs) {
+	public UserLayout(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 		parseAttrs(attrs, 0);
 		onInitialize();
 	}
 
-	public UserView(Context context, AttributeSet attrs, int defStyleAttr) {
+	public UserLayout(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 
 		parseAttrs(attrs, defStyleAttr);
@@ -56,11 +56,11 @@ public class UserView extends View {
 
 	public void parseAttrs(AttributeSet attrs, int defStyleAttr) {
 		// TODO: 24.08.2015 what is defStyleAttr?
-		TypedArray attributes = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.UserView, defStyleAttr, 0);
+		TypedArray attributes = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.UserLayout, defStyleAttr, 0);
 		try
 		{
-			setLifetime( attributes.getInt(R.styleable.UserView_lifetime, LIFETIME_DEAFULT) );
-			int statesColorsResourceId = attributes.getResourceId(R.styleable.UserView_states_colors, 0);
+			setLifetime( attributes.getInt(R.styleable.UserLayout_lifetime, LIFETIME_DEAFULT) );
+			int statesColorsResourceId = attributes.getResourceId(R.styleable.UserLayout_states_colors, 0);
 			TypedArray colorsResources = getResources().obtainTypedArray(statesColorsResourceId);
 			int[] colorsInts = new int[colorsResources.length()];
 			for (int i = 0; i < colorsResources.length(); ++i) {
@@ -86,6 +86,8 @@ public class UserView extends View {
 		int[] colors = new int[]{userBornColor, userAliveColor, userDeadColor};*/
 		//setGradientStatesColors(colors);
 		//setLifetime(lifetime);
+
+		setWillNotDraw(false);
 
 		borderPaint = new Paint();
 		borderPaint.setColor(Color.BLACK);
@@ -176,8 +178,6 @@ public class UserView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-
 		RectF rect = new RectF();
 		rect.left = padding + gradientOffset;
 		rect.right = width - 1 - padding + gradientOffset;
@@ -190,6 +190,8 @@ public class UserView extends View {
 		canvas.drawRect(rect, fillPaint);
 
 		canvas.restore();
-		canvas.drawRect(0, 0, width - 1, height - 1, borderPaint);
+		canvas.drawRect(1, 1, width - 2, height - 2, borderPaint);
+
+		super.onDraw(canvas);
 	}
 }

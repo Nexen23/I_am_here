f2df227bd914adc42098eb60fc77c0d659afd940
Nexen23 <tsalexn@mail.ru>
2015-09-06 00:14:46 +0700

    added dependencies for Google Analytics, Acra. Added config file of GA to .gitignore

--- app/src/main/java/alex/imhere/ParseApplication.java
+++ app/src/main/java/alex/imhere/ParseApplication.java
@@ -2,16 +2,30 @@ package alex.imhere;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseUser;
 
 public class ParseApplication extends Application {
+	public static GoogleAnalytics analytics;
+	public static Tracker tracker;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
+		// Google analytics
+		analytics = GoogleAnalytics.getInstance(this);
+		analytics.setLocalDispatchPeriod(1800);
+
+		tracker = analytics.newTracker("UA-000-1"); // Replace with actual tracker id
+		tracker.enableExceptionReporting(true);
+		tracker.enableAdvertisingIdCollection(true);
+		tracker.enableAutoActivityTracking(true);
+
+		// Parse.com
 		Parse.initialize(this, "ckJRRjvnaJFT5dMkyCXaCOcDWSEHIhOvpviSLz0T", "F19u2qI3lYOBZo8Rr09gYydrZnQP9Nc0PXOnyYro");
 
 		ParseUser.enableAutomaticUser();
